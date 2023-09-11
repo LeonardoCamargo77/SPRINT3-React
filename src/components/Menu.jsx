92a42db6737c4de162223001bbdeb7e3fbeca686
LeonardoCamargo77 <leonardocamargx@gmail.com>
2023-09-10 22:41:04 -0300

    Criação da pasta components + Links navegaveis no menu

---
+++ src/components/Menu.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom"
+
+export default function Menu(){
+
+
+    return(
+        <nav className="menu">
+            <Link to='/Home'>GuinchoSeguro</Link>
+            <span> | </span>
+            <Link to='/Home'>Home</Link>
+            <span> | </span>
+            <Link to='/ChatBot'>ChatBot</Link>
+            <span> | </span>   
+            <Link to='/QuemSomos'>Quem Somos</Link>
+            <span> | </span>          
+
+
+
+        </nav>
+    )
+
+}

69a7b4cb6e2c3941957f28e3f84c64602f841ead
LeonardoCamargo77 <leonardocamargx@gmail.com>
2023-09-10 23:02:42 -0300

    Estilização do site

---
+++ src/components/Rodape.jsx
@@ -0,0 +1,7 @@
+export default function Rodape(){
+    return(
+        <footer>
+            <p>Todos os direitos reservados</p>
+        </footer>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
     errorElement: <Error/>,
     children:[
       {
-        path:"/",
+        path:"/Home",
         element: <Home/>
       },
       {

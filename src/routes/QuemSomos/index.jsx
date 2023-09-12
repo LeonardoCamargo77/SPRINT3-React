1fa7e93358a36c41180db64be87b7b6eb6f5b692
LeonardoCamargo77 <leonardocamargx@gmail.com>
2023-09-12 20:29:00 -0300

    Criação da responsividade do site.

--- src/routes/QuemSomos/index.jsx
+++ src/routes/QuemSomos/index.jsx
@@ -3,11 +3,13 @@ export default function QuemSomos() {
         <main>
             <h1>Integrantes</h1>
             <div className="integrantes">
-                <h3>Leonardo Lucena - 552537</h3>
+                <h3> Leonardo Lucena - 552537</h3>
+                <h3>Ana Paula Nascimento Silva - 552513</h3>
                 <h3>Nathan Nunes Calsonari - 552539</h3>
-                <h3>Ana Paula - 552513</h3>
-                <h3>Calina Thalya - 552523</h3>
-                <h3>Geovana - 99646</h3>
+                <h3>Geovana Ribeiro Domingos - 99646</h3>
+                <h3>Calina Thalya Santana da Silva - 552523</h3>
+              
+
             </div>
             <h1 className="repositorio">Link Repositório:</h1>
             <a href="https://github.com/LeonardoCamargo77/SPRINT3-React.git">Repositório Sprint3</a>

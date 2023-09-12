3a6d1b6e19d2d8a18e08c28719d58edaf3ee3351
anapaulans <rm552513@fiap.com.br>
2023-09-11 21:40:57 -0300

    Adicionando imagens e conteúdo do Rodapé.

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -1,6 +1,7 @@
 export default function Rodape(){
     return(
         <footer>
+            <img className="icones" src="/git.png"/>
             <p>Todos os direitos reservados</p>
             
         </footer>
--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -2,18 +2,13 @@ export default function Home() {
     return(
         <main>
             <h1>Tipos de guinchos</h1>
-            <div class="box">
-            <img src='https://www.tghidraulicos.com.br/media/sig_xnIfhM3iay/produtos/plataforma-hidraulica-para-caminhao/plataforma-hidraulica-para-caminhao-01.jpeg' alt="AsaDeltaPesado"/>
-                <span>Guincho asa delta pesado</span>
+            <div class="Home">
                 
-                
-            <img src='https://img.olx.com.br/images/99/990007019839486.jpg' alt="Cegonha"/>
-                <span>Guincho cegonha</span>
-                
-                
-            <img src='https://www.engetruck.com/inc/scripts/thumbs.php?w=150&h=150&imagem=https://www.engetruck.com/imagens/informacoes//caminhao-guincho-preco-01.jpg' alt="Reboque"/>
-                <span>Guincho reboque</span>
-                </div>
+            <img src='/guinchoasadelta.png' alt="AsaDeltaPesado"/>
+            <img src='/guinchocegonha2.png' alt="Cegonha"/>
+            <img src='/guinchoreboque2.png' alt="Reboque"/>
+         
+            </div>
         </main>
     )
 }

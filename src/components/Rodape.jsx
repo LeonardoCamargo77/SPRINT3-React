395c71e3681f8805f6bd92b033facb953d95973c
NathanNunesCalsonari <rm552539@fiap.com.br>
2023-09-11 22:31:30 -0300

    Ajustes Rodapé + Estilização

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -1,8 +1,15 @@
 export default function Rodape(){
     return(
         <footer>
+           
+            
             <img className="icones" src="/git.png"/>
-            <p>Todos os direitos reservados</p>
+            <img className="icones" src="/face.png"/>
+            <img className="icones" src="/twitter.png"/>
+            <img className="icones" src="/porto3.png"/>
+            
+      
+            
             
         </footer>
     )

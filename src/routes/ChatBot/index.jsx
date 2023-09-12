89d603c3beb22092a68afadc88120fa6142aacb9
calinathalya <calina.thalya@icloud.com>
2023-09-11 21:38:46 -0300

    Adicionando imagens + Estilização do site

--- src/routes/ChatBot/index.jsx
+++ src/routes/ChatBot/index.jsx
@@ -4,7 +4,7 @@ export default function ChatBot() {
             
                 <h1>ChatBot</h1>
                 
-                <img src="https://media.istockphoto.com/id/1161114170/pt/vetorial/email-line-icon-on-black-background-black-flat-style-vector-illustration.jpg?s=170667a&w=0&k=20&c=tdBlee73QRUwXHG66REWXFIoFBU-d-h5T_o7aXWo0IA=" alt="Mensagem"/>
+                <img className="chatbot" src="/chatbot2.png" alt="Mensagem"/>
                 <h3>O ChatBot será a primeira etapa para o início do processo de
                      solicitação de guincho. O segurado deverá enviar mensagens via Telegram informando seus dados básicos como Nome, CPF, RG e número de apólice, em seguida, ele será encaminhado para o GuinchoSeguro, onde deverá selecionar as características que mais se adequal ao seu veículo, para que elas possam ser analisadas antes do envio do guincho.</h3>
 
